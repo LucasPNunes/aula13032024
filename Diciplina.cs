b1bc058bd541e12ebddc0cf61bed799969194a44
Lucas Pimentel Nunes <111577567+LucasPNunes@users.noreply.github.com>
2024-03-13 08:44:48 -0300

    Add files via upload

---
+++ Diciplina.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula13032024
+{
+    internal class Diciplina
+    {
+        private string nome { get ; set; }
+        private int id { get; }
+        private Professor professor { get; set; }
+        private List<int> prerequisitos { get; set; }
+        public Diciplina(string nome, int id)
+        {
+            this.nome = nome;
+            this.id = id;
+            this.prerequisitos = new List<int>();
+        }
+
+        public void addPreRequisito(Diciplina d)
+        {
+            this.prerequisitos.Add(d.id);
+        } 
+    }
+}
---
+++ Professor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula13032024
+{
+    internal class Professor
+    {
+        private string nome { get; set; }
+        private int id { get; }
+        public Professor(string nome, string id)
+        {
+        }
+    }
+}
